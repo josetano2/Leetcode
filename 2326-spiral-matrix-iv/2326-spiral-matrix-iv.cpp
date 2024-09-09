32d53ed44cfdd95b81ceb17fe6db86e47f05d9cf
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-09 10:53:25 +0700

    Time: 175 ms (25.89%), Space: 130.7 MB (17.37%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int height, int width, ListNode* head) {
+        vector<vector<int>> ans(height, vector<int>(width, -1));
+
+        int top = 0, bottom = height - 1;
+        int left = 0, right = width - 1;
+        int x = 0, y = 0;
 
         ListNode* curr = head;
 
-        vector<vector<int>> ans(height, vector<int>(width, -1));
-
-        int tracker = 0, x = tracker, y = tracker;
+        // Directions: right, down, left, up
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int dir = 0; // Start by moving right
 
         while (curr) {
-
-            if (ans[y][x] != -1) {
-                height--;
-                width--;
-                tracker++;
-                x = y = tracker;
-            }
-
-            ans[y][x] = curr->val;
-            cout << x << " " << y << " " << ans[y][x] << endl;
-
-            if (x < width - 1 && y == tracker) {
-                x++;
-            } else if (x == width - 1 && y < height - 1) {
-                y++;
-            } else if (x > tracker && y == height - 1) {
-                x--;
-            } else if (x == tracker && y > tracker) {
-                y--;
-            }
-            
+            ans[x][y] = curr->val;
             curr = curr->next;
+
+            int next_x = x + directions[dir].first;
+            int next_y = y + directions[dir].second;
+
+            // Check if we need to change direction
+            if (next_x < top || next_x > bottom || next_y < left || next_y > right || ans[next_x][next_y] != -1) {
+                // Adjust boundaries after completing a side
+                if (dir == 0) top++;        // Moving right
+                else if (dir == 1) right--; // Moving down
+                else if (dir == 2) bottom--; // Moving left
+                else if (dir == 3) left++;   // Moving up
+
+                // Change direction (right -> down -> left -> up -> right...)
+                dir = (dir + 1) % 4;
+
+                // Update to new direction
+                next_x = x + directions[dir].first;
+                next_y = y + directions[dir].second;
+            }
+
+            // Move to the next cell
+            x = next_x;
+            y = next_y;
         }
 
         return ans;
