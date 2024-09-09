7318a225fb18aa1c31abb0bbf0ddc6c764435917
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-09 11:00:44 +0700

    Time: 171 ms (38.14%), Space: 130.7 MB (17.37%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int height, int width, ListNode* head) {
-
         ListNode* curr = head;
         vector<vector<int>> ans(height, vector<int>(width, -1));
         int tracker = 0, x = tracker, y = tracker;
-
         while (curr) {
-
             ans[y][x] = curr->val;
             curr = curr->next;
-
             if (x < width - 1 && y == tracker) {
                 x++;
             } else if (x == width - 1 && y < height - 1) {
@@ -20,17 +16,13 @@ public:
             } else if (x == tracker && y > tracker) {
                 y--;
             }
-
             if (ans[y][x] != -1) {
                 height--;
                 width--;
                 tracker++;
-
                 x = y = tracker;
-            }
-            
+            }   
         }
-
         return ans;
     }
 };
