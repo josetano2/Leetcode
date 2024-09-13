3beead680590a32a9b21d9a80e8932a414dea55c
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-13 10:53:38 +0700

    Time: 70 ms (33.66%), Space: 44.7 MB (24.27%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+
+    // brute force approach
+    // vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+    //     vector<int> ans;
+    //     for(auto query : queries){
+
+    //         int left = query[0], right = query[1], total = 0;
+    //         while(left <= right){
+    //             total = total ^ arr[left];
+    //             left++;
+    //         }
+
+    //         ans.push_back(total);
+    //     }
+    //     return ans;
+    // }
+
+    // optimized
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        
+       
+        int len = arr.size();
+        int xors[len];
+        xors[0] = arr[0];
+
         vector<int> ans;
 
-        for(auto query : queries){
-
-            int left = query[0], right = query[1], total = 0;
-            while(left <= right){
-                total = total ^ arr[left];
-                left++;
-            }
-
-            ans.push_back(total);
+        for(int i = 1; i < len; i++){
+            xors[i] = xors[i - 1] ^ arr[i];     
+        }
+        
+        for(auto q : queries){
+            if(q[1] - q[0] == 0) ans.push_back(arr[q[1]]);
+            else if(q[0] == 0) ans.push_back(xors[q[1]]);
+            else ans.push_back(xors[q[1]] ^ xors[q[0] - 1]);
         }
         return ans;
     }
