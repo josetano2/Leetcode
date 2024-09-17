ee45385d66b844c6036c88052d0cc84111f5cf06
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-17 10:16:23 +0700

    Time: 2 ms (51.8%), Space: 9.3 MB (19.13%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string, int> map1, map2;
+        int len1 = s1.size(), len2 = s2.size();
+
+        string temp = "";
+        for(int i = 0; i < len1; i++){
+            if(s1[i] != ' ' && i != len1 - 1) temp.push_back(s1[i]);
+            else{
+                if(i == len1 - 1) temp.push_back(s1[i]);
+                map1[temp]++;
+                temp = "";
+            };
+        }
+
+        temp = "";
+        for(int i = 0; i < len2; i++){
+            if(s2[i] != ' ' && i != len2 - 1) temp.push_back(s2[i]);
+            else{
+                if(i == len2 - 1) temp.push_back(s2[i]);
+                map2[temp]++;
+                temp = "";
+            };
+        }
+
+
+        vector<string> ans;
+        for(auto x : map1){
+            if(x.second == 1 && map2[x.first] == 0) ans.push_back(x.first);
+        }
+        for(auto x : map2){
+            if(x.second == 1 && map1[x.first] == 0) ans.push_back(x.first);
+        }
+
+        return ans;
+    }
+};
