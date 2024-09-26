107a4b03a6ebfe185b2fc56828f09ec77d1013c5
josetano2 <132138270+josetano2@users.noreply.github.com>
2024-09-26 21:15:44 +0700

    Time: 89 ms (44.31%), Space: 41.7 MB (73.44%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,21 +1,13 @@
 class MyCalendar {
-private:
-    vector<pair<int, int>> events;
 public:
-    MyCalendar() {
-        
-    }
-    
+    vector<pair<int, int>> events;
     bool book(int start, int end) {
         int len = events.size();
-
         for(int i = 0; i < len; i++){
             if(end > events[i].first && start < events[i].second){
                 return false;
             }
         }
-
-
         events.push_back(make_pair(start, end));
         return true;
     }
