0c783ee4b0a40c684edbbea707d46d871a5ff81a
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-22 15:01:41 +0700

    Time: 61 ms (20.04%), Space: 59.3 MB (71.69%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,7 +3,6 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         int l = 0, r = 0, len = nums.length, ans = 0, temp = 0;
-
         while (r < len) {
 
             if(map.get(nums[r]) == null) {
@@ -13,7 +12,6 @@ class Solution {
             }
             else{
                 ans = Math.max(ans, temp);
-                
                 int lastIdx = map.get(nums[r]);
                 while(l <= lastIdx) {
                     temp -= nums[l];
@@ -21,18 +19,6 @@ class Solution {
                 }
                 map.remove(nums[r]);
             }
-
-            // if (!set.contains(nums[r])) {
-            //     set.add(nums[r]);
-            //     temp += nums[r];
-            //     r++;
-            // } else {
-            //     ans = Math.max(ans, temp);
-            //     set.clear();
-            //     l = r - 1;
-            //     temp = nums[l];
-
-            // }
         }
         return Math.max(ans, temp);
     }
