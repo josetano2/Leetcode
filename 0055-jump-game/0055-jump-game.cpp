b1baa02525f04bf27edfab772b2af52423988b9a
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-24 14:51:50 +0700

    Time: 0 ms (100%), Space: 52.2 MB (64.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-    vector<int> cache;
+    // Greedy
+    bool canJump(vector<int>& nums) {
+        int len = nums.size();
 
-    bool dp(int idx, vector<int>& nums) {
-
-        if (idx >= nums.size() - 1) {
-            return true;
-        }
-
-        if(cache[idx] != -1) {
-            return cache[idx];
-        }
-        
-        for(int i = 1; i <= nums[idx]; i++) {
-            if(dp(idx + i, nums)) {
-                return cache[idx] = true;
+        int goalDist = len - 1;
+        for(int i = len - 2; i > 0; i--) {
+            if(nums[i] >= goalDist - i) {
+                goalDist = i;
             }
         }
-        return cache[idx] = false;
+
+        return nums[0] >= goalDist;
     }
 
-    bool canJump(vector<int>& nums) {
-        cache = vector<int>(nums.size(), -1);
+    // DP Approach
+    // vector<int> cache;
 
-        bool x = dp(0, nums);
-        return dp(0, nums);
-    }
+    // bool dp(int idx, vector<int>& nums) {
+
+    //     if (idx >= nums.size() - 1) {
+    //         return true;
+    //     }
+
+    //     if(cache[idx] != -1) {
+    //         return cache[idx];
+    //     }
+
+    //     for(int i = 1; i <= nums[idx]; i++) {
+    //         if(dp(idx + i, nums)) {
+    //             return cache[idx] = true;
+    //         }
+    //     }
+    //     return cache[idx] = false;
+    // }
+
+    // bool canJump(vector<int>& nums) {
+    //     cache = vector<int>(nums.size(), -1);
+
+    //     bool x = dp(0, nums);
+    //     return dp(0, nums);
+    // }
 };
