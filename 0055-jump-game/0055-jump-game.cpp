10a7b295913f396b44f83b63bb761e2ceac4808b
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-24 14:20:29 +0700

    Time: 1370 ms (5.01%), Space: 55.7 MB (6.81%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> cache;
+
+    bool dp(int idx, vector<int>& nums) {
+
+        if (idx >= nums.size() - 1) {
+            return true;
+        }
+
+        if(cache[idx] != -1) {
+            return cache[idx];
+        }
+        
+        for(int i = 1; i <= nums[idx]; i++) {
+            if(dp(idx + i, nums)) {
+                return cache[idx] = true;
+            }
+        }
+        return cache[idx] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        cache = vector<int>(nums.size(), -1);
+
+        bool x = dp(0, nums);
+        for(auto x : cache) {
+            cout << x << " ";
+        }
+        return dp(0, nums);
+    }
+};
