a861b9b49b22a2753a9f4c8ed7746be751098b44
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-26 18:29:36 +0700

    Time: 4 ms (18.84%), Space: 12.3 MB (19.27%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,13 +4,7 @@ public:
     vector<vector<int>> ans;
 
     void backtrack(int idx, vector<int>& curr, vector<int>& nums) {
-        cout << "Index: " << idx << endl;
-        for (auto x : curr) {
-            cout << x << " ";
-        }
-        cout << endl;
         if (idx >= nums.size() && !unique.count(curr)) {
-            cout << "MASUK" << endl;
             ans.push_back(vector<int>(curr));
             unique.insert(vector<int>(curr));
             return;
@@ -18,13 +12,10 @@ public:
 
         if (idx >= nums.size())
             return;
-
-        cout << "Push" << endl << endl;
         
         curr.push_back(nums[idx]);
         backtrack(idx + 1, curr, nums);
 
-        cout << "Pop" << endl << endl;
         curr.pop_back();
         backtrack(idx + 1, curr, nums);
     }
