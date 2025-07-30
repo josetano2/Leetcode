74d36497daea129d6defd111cd13917b00b9e353
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-30 22:22:56 +0700

    Time: 2 ms (44.1%), Space: 17.1 MB (87.83%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int maxProfit = 0, len = prices.size(), currHold = -1;
+
+        for(int i = 0; i < len - 1; i++){
+
+            if(prices[i] < prices[i + 1] && currHold == -1) {
+                currHold = prices[i];
+                cout << currHold;
+            }
+
+            if(prices[i] > prices[i + 1] && currHold != -1) {
+                maxProfit += (prices[i] - currHold);
+                currHold = -1;
+            }
+
+        }
+
+        if(currHold != -1) {
+            maxProfit += (prices[len - 1] - currHold);
+        }
+        
+        return maxProfit;
+    }
+};
