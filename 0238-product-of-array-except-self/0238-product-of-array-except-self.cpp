8b7e9af2c83c7666054244aa03336d47035456ef
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-31 16:49:20 +0700

    Time: 0 ms (100%), Space: 41.6 MB (38.14%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ans;
-        int tot = 1, len = nums.size(), zeroCounter = 0;
 
+        int total = 1, zeroCounter = 0;
 
-        for(int i = 0; i < len; i++){
-            if(nums[i] != 0){
-                tot *= nums[i];
-            }
-            else{
+        for (auto x : nums) {
+            if (x == 0) {
                 zeroCounter++;
+                continue;
             }
+            total *= x;
         }
 
-        // cout << tot;
-
-        for(int i = 0; i < len; i++){
-            if(zeroCounter > 1 || (zeroCounter >= 1 && nums[i] != 0)){
-                ans.push_back(0);
-            }
-            // nums[i] = 0
-            // 
-            else if(zeroCounter == 1 && nums[i] == 0){
-                ans.push_back(tot);
-            }
-            else{
-                ans.push_back(tot/nums[i]);
+        vector<int> ans;
+        for (auto x : nums) {
+            if (x != 0) {
+                if (zeroCounter > 0) {
+                    ans.push_back(0);
+                } else {
+                    ans.push_back(total / x);
+                }
+            } else {
+                if (zeroCounter == 1) {
+                    ans.push_back(total);
+                } else {
+                    ans.push_back(0);
+                }
             }
         }
-
         return ans;
     }
 };
