cc1470bdbcebe45797d614fb52e84b9018a45edf
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-07-31 16:49:29 +0700

    Time: 0 ms (100%), Space: 40.2 MB (57.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
+
+    // Without Division
     vector<int> productExceptSelf(vector<int>& nums) {
 
-        int total = 1, zeroCounter = 0;
+        vector<int> ans(nums.size(), 1);
 
-        for (auto x : nums) {
-            if (x == 0) {
-                zeroCounter++;
-                continue;
-            }
-            total *= x;
+        int prefixSum = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            prefixSum *= nums[i - 1];
+            ans[i] = prefixSum;
         }
 
-        vector<int> ans;
-        for (auto x : nums) {
-            if (x != 0) {
-                if (zeroCounter > 0) {
-                    ans.push_back(0);
-                } else {
-                    ans.push_back(total / x);
-                }
-            } else {
-                if (zeroCounter == 1) {
-                    ans.push_back(total);
-                } else {
-                    ans.push_back(0);
-                }
-            }
+        prefixSum = 1;
+        for(int i = nums.size() - 1; i > 0; i--) {
+            prefixSum *= nums[i];
+            ans[i - 1] *= prefixSum;
         }
+
         return ans;
     }
+
+    // With Division
+    // vector<int> productExceptSelf(vector<int>& nums) {
+
+    //     int total = 1, zeroCounter = 0;
+
+    //     for (auto x : nums) {
+    //         if (x == 0) {
+    //             zeroCounter++;
+    //             continue;
+    //         }
+    //         total *= x;
+    //     }
+
+    //     vector<int> ans;
+    //     for (auto x : nums) {
+    //         if (x != 0) {
+    //             if (zeroCounter > 0) {
+    //                 ans.push_back(0);
+    //             } else {
+    //                 ans.push_back(total / x);
+    //             }
+    //         } else {
+    //             if (zeroCounter == 1) {
+    //                 ans.push_back(total);
+    //             } else {
+    //                 ans.push_back(0);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
