d77faaee0cca8afd8dfefda163a3c19eb82c9a58
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-08-02 15:55:37 +0700

    Time: 6 ms (0.31%), Space: 14 MB (79.45%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,56 +1,29 @@
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int len = nums.size();
+        
+        int l = 0, r = nums.size() - 1, mid = 0;
 
-        int left = 0, right = len - 1, mid = 0;
+        // Return L if smaller then right side
+        while(l < r) {
+            mid = l + ((r - l) / 2);
 
-        // 3 4 5 1 2 mid > left && mid > right -> left = mid + 1
-        // 4 5 1 2 3 mid < left && mid < right -> 
-        // 5 1 2 3 4 mid < left && mid < right -> right = mid
-        // 2 3 4 5 1 mid > left && mid > right -> left = mid + 1
+            cout << l << " " << mid << " " << r << endl;
 
-        // 3 1 2 mid < left && mid < right
-        // 3 1 mid >= left && mid > right
-
-        while(left != right){
-            mid = left + (right - left) / 2;
-
-            cout << "Left Idx: " << left << endl;
-            cout << "Mid Idx: " << mid << endl;
-            cout << "Right Idx: " << right << endl << endl;
-            cout << "Left: " << nums[left] << endl;
-            cout << "Mid: " << nums[mid] << endl;
-            cout << "Right: " << nums[right] << endl << endl;
-
-            if(nums[left] <= nums[right]){
-                return nums[left];
+            if(nums[mid] > nums[r]) {
+                l = mid + 1;
             }
 
-            if(nums[mid] >= nums[left] && nums[mid] > nums[right]){
-                left = mid + 1;
-            }
-            else if(nums[mid] < nums[left] && nums[mid] < nums[right]){
-                right = mid;
-            }
-            else{
-                left = mid + 1;
+            else if(nums[mid] < nums[r]) {
+                r = mid;
             }
 
-            cout << "Left Idx: " << left << endl;
-            cout << "Mid Idx: " << mid << endl;
-            cout << "Right Idx: " << right << endl << endl;
-            cout << "Left: " << nums[left] << endl;
-            cout << "Mid: " << nums[mid] << endl;
-            cout << "Right: " << nums[right] << endl << endl;
-
-            cout << "======" << endl;
-
+            if(nums[l] < nums[r]) {
+                return nums[l];
+            }
         }
 
-        int ans = min(nums[left], nums[right]);
-
-        return min(ans, nums[mid]);
+        return nums[l];
+        
     }
 };
