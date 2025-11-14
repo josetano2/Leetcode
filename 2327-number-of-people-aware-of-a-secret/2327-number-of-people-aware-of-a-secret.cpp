7481ba0a3cdc58abbe5206aaceaf0c675bbaae59
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-11-14 13:10:02 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long long int> dp(n + 1, 0);
+
+        dp[1] = 1;
+        long long int know = 0;
+        for (int i = 2; i <= n; i++) {
+            int delayIdx = i - delay >= 0 ? i - delay : 0;
+            int forgetIdx = i - forget >= 0 ? i - forget : 0;
+            long long int spread = dp[delayIdx];
+            long long int forget = dp[forgetIdx];
+
+            cout << know + spread - forget << endl;
+            dp[i] = (know + spread - forget) % 1000000007;
+            know = (know + spread - forget) % 1000000007;
+        }
+
+        long long int tot = 0;
+        for(int i = n - forget + 1; i <= n; i++) tot += dp[i];
+
+        return tot % 1000000007;
+    }
+};
