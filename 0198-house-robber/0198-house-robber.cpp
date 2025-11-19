4e3a997a2919bf302a84724170b8071a407d0277
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-11-19 11:41:32 +0700

    Time: 0 ms (100%), Space: 13.1 MB (5.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> cache;
+
+    int dp(vector<int> nums, int idx) {
+        if(idx >= nums.size()) return 0;
+
+        if(cache[idx] != -1) return cache[idx];
+
+        return cache[idx] = max(nums[idx] + dp(nums, idx + 2), dp(nums, idx + 1));
+    }
+
+    int rob(vector<int>& nums) {
+        cache = vector<int>(nums.size(), -1);
+        dp(nums, 0);
+        return cache[0];
+    }
+};
