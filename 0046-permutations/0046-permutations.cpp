cb6efbd5113dca35036b4f1c036a700849a3ec31
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-11-20 17:25:47 +0700

    Time: 3 ms (28.99%), Space: 10.7 MB (38.62%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
 
-    void backtrack(vector<vector<int>>& ans, vector<int>& curr, vector<int>& nums) {
+    void backtrack(vector<vector<int>>& ans, vector<int>& curr, vector<int>& nums, vector<bool>& cache) {
         if(curr.size() == nums.size()) {
             ans.push_back(curr);
             return;
         }
 
         for(int i = 0; i < nums.size(); i++) {
-            if(find(curr.begin(), curr.end(), nums[i]) != curr.end()) continue;
+            if(cache[i]) continue;
+
+            cache[i] = true;
             curr.push_back(nums[i]);
-            backtrack(ans, curr, nums);
+            backtrack(ans, curr, nums, cache);
             curr.pop_back();
+            cache[i] = false;
         }
     
     }
@@ -19,7 +22,8 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> curr;
-        backtrack(ans, curr, nums);
+        vector<bool> cache(nums.size(), false);
+        backtrack(ans, curr, nums, cache);
         return ans;
     }
 };
