db06c0fe48fccab4312c2298452289c6b818c563
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-11-20 14:44:12 +0700

    Time: 0 ms (100%), Space: 28 MB (64.58%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int count = 1;
     bool isValid(vector<vector<int>>& grid, vector<vector<bool>>& visited,
                  int x, int y) {
         if (x < 0 || x >= grid[0].size() || y < 0 || y >= grid.size() ||
             visited[y][x] || grid[y][x] == 0)
             return false;
-        count++;
         return true;
     }
 
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int x,
+    int dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int x,
              int y) {
         visited[y][x] = true;
+        int area = 1;
         if (isValid(grid, visited, x + 1, y))
-            dfs(grid, visited, x + 1, y);
+            area += dfs(grid, visited, x + 1, y);
         if (isValid(grid, visited, x - 1, y))
-            dfs(grid, visited, x - 1, y);
+            area += dfs(grid, visited, x - 1, y);
         if (isValid(grid, visited, x, y + 1))
-            dfs(grid, visited, x, y + 1);
+            area += dfs(grid, visited, x, y + 1);
         if (isValid(grid, visited, x, y - 1))
-            dfs(grid, visited, x, y - 1);
+            area += dfs(grid, visited, x, y - 1);
+        
+        return area;
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -28,14 +29,10 @@ public:
         int y = grid.size();
         int ans = 0;
         vector<vector<bool>> visited(y, vector<bool>(x, false));
-        int tot = 0;
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
                 if (!visited[i][j] && grid[i][j] == 1) {
-                    tot++;
-                    count = 1;
-                    dfs(grid, visited, j, i);
-                    ans = max(ans, count);
+                    ans = max(ans, dfs(grid, visited, j, i));
                 }
             }
         }
