365824d83b68a2a6bee68da2c1a44678a7612d56
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-02 13:56:50 +0700

    Time: 153 ms (31.84%), Space: 221.8 MB (10.79%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int, vector<int>> freq;
+        long long int MOD = 1e09 + 7;
+
+        for (auto& z : points) freq[z[1]].push_back(z[0]);
+
+        vector<pair<int, int>> horizontals;
+
+        for (auto& f : freq) {
+            if (f.second.size() >= 2) {
+                horizontals.push_back({ f.first, f.second.size() });
+            }
+        }
+        long long sum = 0, squareSum = 0;
+        for(int i = 0; i < horizontals.size(); i++) {
+            int len = horizontals[i].second;
+
+            long long way = (len % MOD * (len % MOD - 1) % MOD) / 2;
+            sum += way;
+            squareSum += way * way;
+        }  
+        return ((sum * sum - squareSum) / 2) % MOD;
+    }
+};
