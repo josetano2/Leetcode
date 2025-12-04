99267ee038a3208bb2e0850da8c2b56fd4508845
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-04 14:42:22 +0700

    Time: 31 ms (84.52%), Space: 53.5 MB (80.04%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -9,26 +9,26 @@ public:
         return true;
     }
 
-    void backtrack(string& s, int j, int i, vector<string>& curr, vector<vector<string>>& ans) {
+    void backtrack(string& s, int i, vector<string>& curr, vector<vector<string>>& ans) {
 
-        if (i >= s.size()) {
-            if (i == j) ans.push_back(curr);
+        if (i == s.size()) {
+            ans.push_back(curr);
             return;
         }
 
-        if (isValid(s, j, i)) {
-            curr.push_back(s.substr(j, i - j + 1));
-            backtrack(s, i + 1, i + 1, curr, ans);
-            curr.pop_back();
+        for(int j = i; j < s.size(); j++) {
+            if (isValid(s, i, j)) {
+                curr.push_back(s.substr(i, j - i + 1));
+                backtrack(s, j + 1, curr, ans);
+                curr.pop_back();
+            }
         }
-
-        backtrack(s, j, i + 1, curr, ans);
     }
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> curr;
-        backtrack(s, 0, 0, curr, ans);
+        backtrack(s, 0, curr, ans);
         return ans;
     }
 };
