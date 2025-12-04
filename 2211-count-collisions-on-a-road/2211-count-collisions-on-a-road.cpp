aba836eb87f306d72bc80bcebf0678670e172bc8
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-04 10:59:29 +0700

    Time: 56 ms (13.91%), Space: 19.6 MB (49.86%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -13,31 +13,26 @@ public:
             // R + L = 1 - 1;
             int curr = card[directions[i]] + card[directions[i + 1]];
             if (curr == 0 && directions[i] == 'R' && directions[i + 1] == 'L') {
-                cout << i << " ";
                 tot += 2;
                 directions[i] = 'S';
                 directions[i + 1] = 'S';
                 int temp = i - 1;
                 while(temp >= 0 && directions[temp] == 'R') {
-                    cout << temp << " ";
                     tot += 1;
                     directions[temp] = 'S';
                     temp--;
                 }
             } else if (curr == 1 && directions[i] == 'R') {
-                cout << i << " ";
                 tot += 1;
                 directions[i] = 'S';
 
                 int temp = i - 1;
                 while(temp >= 0 && directions[temp] == 'R') {
-                    cout << temp << " ";
                     tot += 1;
                     directions[temp] = 'S';
                     temp--;
                 }
             } else if (curr == -1 && directions[i] == 'S') {
-                cout << i << " ";
                 tot += 1;
                 directions[i + 1] = 'S';
             }
