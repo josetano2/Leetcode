e1f1decbf9a6b6ec821604687f7e3ad3f0cea840
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-04 17:42:56 +0700

    Time: 3 ms (39.05%), Space: 13.9 MB (98.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void backtrack(int target, int idx, int currTotal, vector<int>& candidates, vector<int>& curr, vector<vector<int>>& ans, set<vector<int>>& cache) {
+    void backtrack(int target, int idx, int currTotal, vector<int>& candidates, vector<int>& curr, vector<vector<int>>& ans) {
         if (currTotal == target) {
-            if (!cache.count(curr)) {
-                ans.push_back(curr);
-                cache.insert(curr);
-            }
+            ans.push_back(curr);
             return;
         }
 
-        if (currTotal > target) return;
+        if (currTotal > target)
+            return;
 
         for (int i = idx; i < candidates.size(); i++) {
-            if(i > idx && candidates[i] == candidates[i - 1]) continue;
+            if (i > idx && candidates[i] == candidates[i - 1]) continue;
+
             curr.push_back(candidates[i]);
-            backtrack(target, i + 1, currTotal + candidates[i], candidates, curr, ans, cache);
+            backtrack(target, i + 1, currTotal + candidates[i], candidates, curr, ans);
             curr.pop_back();
         }
     }
@@ -23,8 +22,7 @@ public:
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> curr;
-        set<vector<int>> cache;
-        backtrack(target, 0, 0, candidates, curr, ans, cache);
+        backtrack(target, 0, 0, candidates, curr, ans);
         return ans;
     }
 };
