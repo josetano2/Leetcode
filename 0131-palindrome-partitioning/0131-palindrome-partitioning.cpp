10fab438800721a1f92e1fd94c4b9d99a7f91339
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-04 14:37:07 +0700

    Time: 140 ms (9.54%), Space: 161.4 MB (5.94%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            r--;
+            l++;
+        }
+        return true;
+    }
+
+    void backtrack(string s, int j, int i, vector<string>& curr, vector<vector<string>>& ans) {
+
+        if (i >= s.size()) {
+            if (i == j) ans.push_back(vector<string>(curr));
+            return;
+        }
+
+        if (isValid(s, j, i)) {
+            curr.push_back(s.substr(j, i - j + 1));
+            backtrack(s, i + 1, i + 1, curr, ans);
+            curr.pop_back();
+        }
+
+        backtrack(s, j, i + 1, curr, ans);
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> curr;
+        backtrack(s, 0, 0, curr, ans);
+        return ans;
+    }
+};
