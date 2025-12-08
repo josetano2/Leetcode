48add11c7075d9ce03b347a46641dd24e8d8857b
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-08 15:48:29 +0700

    Time: 0 ms (100%), Space: 30.9 MB (41.21%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+
+        int count = 0, peak = INT_MIN;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                if(count == 0) {
+                    count++;
+                    if(i > 0) {
+                        int prev = nums[i - 1], curr = nums[i], next = nums[i + 1];
+                        if(next >= prev) nums[i] = next;
+                        if(prev > next) nums[i + 1] = curr;
+                    }
+                    if(i == 0) {
+                        nums[i] = nums[i + 1] - 1;
+                    }
+                }
+                else return false;
+            }
+            peak = max(nums[i], peak);
+
+            if(nums[i + 1] < peak && count == 1) return false;
+            
+        }
+        return true;
+    }
+};
