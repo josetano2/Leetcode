cd91ed632e6100261cddcf58089d0595a028e60c
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-08 10:57:16 +0700

    Time: 7 ms (62.17%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -3,13 +3,10 @@ public:
     int countTriples(int n) {
 
         int ans = 0;
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < n; j++) {
-                double x = sqrt(i * i + j * j);
-                if(floor(x) == x && x <= n) {
-                    cout << i * i << " " << j * j << " " << sqrt(i * i + j * j) << endl;
-                    ans++;
-                }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                double x = sqrt(i * i - j * j);
+                if(floor(x) == x && x <= n) ans++;
             }
         }
 
