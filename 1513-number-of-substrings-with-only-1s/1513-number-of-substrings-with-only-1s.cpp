fdd17ac1538559606217901998d2a45df9a3e481
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-10 14:45:50 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int numSub(string s) {
-
-        vector<long long int> cache(100000, 0);
-        cache[0] = 1;
         long long int ans = 0, MOD = 1e09+7;
-        
-        for(int i = 1; i < 100000; i++) cache[i] = (cache[i - 1] + i + 1) % MOD;
-
         for(int i = 0; i < s.size(); i++) {
             if(s[i] == '1') {
                 int curr = i;
@@ -15,12 +9,10 @@ public:
                     curr++;
                 }
                 int diff = curr - i;
-                ans = (ans + cache[diff - 1]) % MOD;
+                ans = (ans + (diff * (diff + 1) / 2)) % MOD;
                 i = curr;
             }
         }
-        
-
         return ans;
     }
 };
