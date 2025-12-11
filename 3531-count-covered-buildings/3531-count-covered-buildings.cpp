3ba07b5e27ac6dee29bcd7e8b6c21fb34c13107e
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-11 13:36:03 +0700

    Time: 869 ms (25.41%), Space: 451.4 MB (17.68%) - LeetHub

---
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+
+        unordered_map<int, set<int>> hashY;
+        unordered_map<int, set<int>> hashX;
+
+        for (auto& coordinates : buildings) {
+            int x = coordinates[0], y = coordinates[1];
+
+            hashX[x].insert(y);
+            hashY[y].insert(x);
+        }
+
+        int ans = 0;
+        for (auto& coordinates : buildings) {
+            int x = coordinates[0], y = coordinates[1];
+
+            if(x < *hashY[y].rbegin() && x > *hashY[y].begin() && y < *hashX[x].rbegin() && y > *hashX[x].begin()) ans++;
+        }
+
+        return ans;
+    }
+};
