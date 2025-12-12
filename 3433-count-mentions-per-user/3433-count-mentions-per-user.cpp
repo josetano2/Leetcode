21aebec3ccaefd73c19a3155f21241ecef42431e
josetano2 <132138270+josetano2@users.noreply.github.com>
2025-12-12 11:00:26 +0700

    Time: 139 ms (17.02%), Space: 45.8 MB (38.3%) - LeetHub

---
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> countMentions(int numberOfUsers, vector<vector<string>>& events) {
+        sort(events.begin(), events.end(), [](const vector<string>& a, const vector<string>& b) {
+            if(stoi(a[1]) == stoi(b[1])) return a[0] > b[0];
+            return stoi(a[1]) < stoi(b[1]);
+        });
+
+        vector<vector<int>> mentions(numberOfUsers, {0, -1});
+        for(auto& event : events) {
+            string msgEvent = event[0];
+            int timestamp = stoi(event[1]);
+            cout << msgEvent << " " << timestamp << endl;
+
+            if(msgEvent == "MESSAGE") {
+                string scope = event[2];
+
+                if(scope == "HERE") {
+                    for(auto& x : mentions) {
+                        if(x[1] <= timestamp || x[1] == -1) x[0]++;
+                    }
+                }
+                else if(scope == "ALL") {
+                    for(auto& x : mentions) {
+                        x[0]++;
+                    }
+                }
+                else {
+                    vector<int> tags;
+                    stringstream ss(scope);
+                    string word;
+
+                    while (ss >> word) tags.push_back(stoi(word.substr(2)));
+
+                    for(auto& x : tags) {
+                        mentions[x][0]++;
+                    }
+                    
+                }
+            }
+            else {
+                int id = stoi(event[2]);
+                mentions[id][1] = timestamp + 60;
+            }
+         }
+
+
+        vector<int> ans;
+        for(auto& x : mentions) ans.push_back(x[0]);
+        return ans;
+        
+    }
+};
