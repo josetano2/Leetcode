a0fc7f9d336ac0bc2bb6835bd8591e85ccb20b96
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-13 16:34:47 +0700

    Time: 194 ms (28.23%), Space: 171.7 MB (31.78%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-
         unordered_map<int, int> freq;
         for(auto& x : nums) freq[x]++;
-
         vector<int> ans;
-        for(auto& x : nums) {
-            if(freq[x] == 1 && freq.count(x - 1) == 0 && freq.count(x + 1) == 0) ans.push_back(x);
-        }
-
+        for(auto& x : nums) if(freq[x] == 1 && freq.count(x - 1) == 0 && freq.count(x + 1) == 0) ans.push_back(x);
         return ans;
     }
 };
