e2c580d3a3ff9332f9461064f5b34b984a859b28
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-14 15:54:52 +0700

    Time: 49 ms (6.5%), Space: 96.7 MB (29.33%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
@@ -6,9 +6,7 @@ public:
         int count = 0;
         for(int i = 0; i < nums.size(); i++) {
             int j = i;
-            while(j < nums.size() && nums[j] - nums[i] <= k) {
-                j++;
-            }
+            while(j < nums.size() && nums[j] - nums[i] <= k) j++;
             i = j - 1;
             count++;
         }
