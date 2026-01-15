85d26bbeff950b881fc97d6c4f2693771b5fffab
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-15 15:23:36 +0700

    Time: 0 ms (100%), Space: 31.9 MB (67.83%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
+
+    int longestStreak(vector<int>& bars) {
+        int curr = 1, b = 1;
+        for(int i = 0; i < bars.size() - 1; i++) {
+            if(bars[i] + 1 == bars[i + 1]) curr++;
+            else {
+                b = max(curr, b);
+                curr = 1;
+            }
+        }
+        return max(curr, b);
+    }
+
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
         // n -> x, m -> y
         sort(hBars.begin(), hBars.end());
         sort(vBars.begin(), vBars.end());
 
-        int tempH = 1, tempV = 1, h = 1, v = 1;
-        for(int i = 0; i < hBars.size() - 1; i++) {
-            if(hBars[i] + 1 == hBars[i + 1]) {
-                tempH++;
-            }
-            else {
-                h = max(tempH, h);
-                tempH = 1;
-            }
-        }
-        for(int i = 0; i < vBars.size() - 1; i++) {
-            if(vBars[i] + 1 == vBars[i + 1]) {
-                tempV++;
-            }
-            else {
-                v = max(tempV, v);
-                tempV = 1;
-            }
-        }
-        h = max(tempH, h);
-        v = max(tempV, v);
-
-        int areaL = min(h, v) + 1;
-        return pow(areaL, 2);
+        int h = longestStreak(hBars), v = longestStreak(vBars);
+        return pow(min(h, v) + 1, 2);
     }
 };
