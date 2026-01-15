31f7863b0ccded5c05cee12ff4ddc015a6210674
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-15 15:28:09 +0700

    Time: 0 ms (100%), Space: 31.9 MB (45.22%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
 
     int longestStreak(vector<int>& bars) {
+        sort(bars.begin(), bars.end());
+
         int curr = 1, b = 1;
         for(int i = 0; i < bars.size() - 1; i++) {
             if(bars[i] + 1 == bars[i + 1]) curr++;
@@ -14,10 +16,6 @@ public:
     }
 
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        // n -> x, m -> y
-        sort(hBars.begin(), hBars.end());
-        sort(vBars.begin(), vBars.end());
-
         int h = longestStreak(hBars), v = longestStreak(vBars);
         return pow(min(h, v) + 1, 2);
     }
