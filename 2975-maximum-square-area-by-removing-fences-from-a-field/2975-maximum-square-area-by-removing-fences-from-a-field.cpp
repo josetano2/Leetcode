b03828668b3e3776f4b12a4d22e2f89076a0ce58
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-16 18:11:58 +0700

    Time: 1835 ms (22.33%), Space: 445.7 MB (28.16%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
+
+    unordered_set<int> generatePossiblePos(int x, vector<int>& fences) {
+        unordered_set<int> possible;
+        possible.insert(x - 1);
+
+        for(int i = 0; i < fences.size(); i++) {
+            possible.insert(fences[i] - 1);
+            possible.insert(x - fences[i]);
+        }
+
+        for(int i = 0; i < fences.size(); i++) {
+            for(int j = i + 1; j < fences.size(); j++) {
+                possible.insert(abs(fences[j] - fences[i]));
+            }
+        }
+
+        return possible;
+    }
+
     int maximizeSquareArea(int n, int m, vector<int>& hFences, vector<int>& vFences) {
         
         long long MOD = 1e09+7;
-        unordered_set<int> possibleW, possibleH;
-        possibleH.insert(n - 1);
-        possibleW.insert(m - 1); 
-
-        for(int i = 0; i < hFences.size(); i++) {
-            possibleH.insert(hFences[i] - 1);
-            possibleH.insert(n - hFences[i]);
-        }
-
-        for(int i = 0; i < vFences.size(); i++) {
-            possibleW.insert(vFences[i] - 1);
-            possibleW.insert(m - vFences[i]);
-        }
-
-        for(int i = 0; i < hFences.size(); i++) {
-            for(int j = i + 1; j < hFences.size(); j++) {
-                possibleH.insert(abs(hFences[j] - hFences[i]));
-            }
-        }
-
-        for(int i = 0; i < vFences.size(); i++) {
-            for(int j = i + 1; j < vFences.size(); j++) {
-                possibleW.insert(abs(vFences[j] - vFences[i]));
-            }
-        }
-        
+        unordered_set<int> possibleW = generatePossiblePos(m, vFences), possibleH = generatePossiblePos(n, hFences);
 
         long long area = -1;
         for(auto& x : possibleH) {
-            if(possibleW.find(x) != possibleW.end()) {
-                if(area < x) area = x;
-            }
+            if(possibleW.find(x) != possibleW.end()) if(area < x) area = x;
         }
-
         return area == -1 ? area : (area * area) % MOD;
     }
 };
