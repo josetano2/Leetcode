b3699d81ff5b183cb9789349de12e1ecf3f65fb2
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-16 18:17:20 +0700

    Time: 1716 ms (37.86%), Space: 445.8 MB (11.65%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -5,26 +5,16 @@ public:
         unordered_set<int> possible;
         fences.push_back(1);
         fences.push_back(x);
-
         sort(fences.begin(), fences.end());
-        for(int i = 0; i < fences.size(); i++) {
-            for(int j = i + 1; j < fences.size(); j++) {
-                possible.insert(fences[j] - fences[i]);
-            }
-        }
-
+        for(int i = 0; i < fences.size(); i++) for(int j = i + 1; j < fences.size(); j++) possible.insert(fences[j] - fences[i]);
         return possible;
     }
 
     int maximizeSquareArea(int n, int m, vector<int>& hFences, vector<int>& vFences) {
-        
         long long MOD = 1e09+7;
         unordered_set<int> possibleW = generatePossiblePos(m, vFences), possibleH = generatePossiblePos(n, hFences);
-
         long long area = -1;
-        for(auto& x : possibleH) {
-            if(possibleW.find(x) != possibleW.end()) if(area < x) area = x;
-        }
+        for(auto& x : possibleH) if(possibleW.find(x) != possibleW.end()) if(area < x) area = x;
         return area == -1 ? area : (area * area) % MOD;
     }
 };
