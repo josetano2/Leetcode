b7bf6f7818fd31a2a66a2ec504f36c8a8616b88a
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-01-26 11:22:53 +0700

    Time: 17 ms (36.07%), Space: 36.6 MB (64.03%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int minDiff = INT_MAX;
-        unordered_set<int> freq;
-        vector<vector<int>> ans;
-        for(int i = 0; i < arr.size() - 1; i++) {
-            minDiff = min(minDiff, abs(arr[i + 1] - arr[i]));
-            freq.insert(arr[i]);
-        }
-        freq.insert(arr[arr.size() - 1]);
-
-        for(int i = 0; i < arr.size(); i++) {
-            int toSearch = minDiff + arr[i];
-            if(freq.find(toSearch) != freq.end()) {
-                ans.push_back({arr[i], toSearch});
-                freq.erase(toSearch); freq.erase(arr[i]);
-            }
-        }
-    
+        sort(arr.begin(), arr.end()); int minDiff = INT_MAX; vector<vector<int>> ans;
+        for(int i = 0; i < arr.size() - 1; i++) minDiff = min(minDiff, abs(arr[i + 1] - arr[i]));
+        for(int i = 0; i < arr.size() - 1; i++) if(arr[i + 1] - arr[i] == minDiff) ans.push_back({arr[i], arr[i + 1]});
         return ans;
     }
 };
