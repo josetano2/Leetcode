5c4ab0eb0c0710dae9eaf8d29f53687aa90bc24a
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-11 19:32:15 +0700

    Time: 1575 ms (6.72%), Space: 406.7 MB (6.43%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkSubSeq(string s, string p, unordered_set<int>& skipped) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < p.size()) {
+            if(skipped.count(i)) {
+                i++; continue;
+            }
+            if (s[i] == p[j]) j++;
+            i++;
+        }
+        return j == p.size();
+    }
+
+    int maximumRemovals(string s, string p, vector<int>& removable) {
+        int l = 0, r = removable.size() - 1, mid = 0;
+
+        unordered_set<int> removed;
+        while(l <= r) {
+            mid = l + (r - l) / 2;
+
+            removed = unordered_set<int>(removable.begin(), removable.begin() + mid + 1);
+            cout << l << " " << mid << " " << r << " " << checkSubSeq(s, p, removed) << endl;
+            if(checkSubSeq(s, p, removed)) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        cout << l << " " << mid << " " << r << " " << checkSubSeq(s, p, removed) << endl;
+
+        return l;
+    }
+};
