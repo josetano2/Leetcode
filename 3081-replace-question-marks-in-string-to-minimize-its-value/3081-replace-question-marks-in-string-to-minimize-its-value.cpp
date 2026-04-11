35f8ddf1c8ef9061b95faf0e90fb2453dca82dc4
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-11 20:23:09 +0700

    Time: 96 ms (42.31%), Space: 23.1 MB (37.69%) - LeetHub

---
+++ 3081-replace-question-marks-in-string-to-minimize-its-value/3081-replace-question-marks-in-string-to-minimize-its-value.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    struct comp {
+        bool operator()(pair<char, int> a, pair<char, int> b) {
+            if (a.second == b.second) return a.first > b.first;
+            return a.second > b.second;
+        }
+    };
+
+    string minimizeStringValue(string s) {
+        unordered_map<char, int> freq;
+        for(int i = 'a'; i <= 'z'; i++) freq[i] = 0;
+
+        for(auto& x : s) freq[x]++;
+
+        priority_queue<pair<char, int>, vector<pair<char, int>>, comp> pq;
+
+        for(auto& [x, y] : freq) {
+            if(x == '?') continue;
+            pq.push({x, y});
+        }
+
+        int totalQuestion = freq['?'];
+
+        string filled = "";
+        while(totalQuestion > 0) {
+            pair<char, int> top = pq.top();
+            pq.pop();
+            filled += top.first;
+            pq.push({top.first, top.second + 1});
+            totalQuestion--;
+        }
+
+        sort(filled.begin(), filled.end());
+        int curr = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '?') {
+                s[i] = filled[curr]; curr++;
+            }
+        }
+        auto temp = pq;
+        while (!temp.empty()) {
+            auto [count, ch] = temp.top();
+            cout << ch << " " << count << endl;
+            temp.pop();
+        }
+    
+        return s;
+    }
+};
