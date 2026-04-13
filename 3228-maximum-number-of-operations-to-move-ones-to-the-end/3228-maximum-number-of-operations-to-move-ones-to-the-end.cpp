9763880aa995f31bb88b12250a3bb45646147b1d
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-13 21:25:35 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,53 +1,11 @@
 class Solution {
 public:
-
-    int findNextOne(string& s, int curr) {
-        while(curr < s.size()) {
-            if(s[curr] == '1') return curr;
-            curr++;
-        }
-        return -1;
-    }
-
-    int findTotalBundledOne(string& s, int curr) {
-        int total = 0;
-        while(curr < s.size()) {
-            if(s[curr] != '1') break;
-            curr++; total++;
-        }
-        return total;
-    }
-
-    int totalOne(string& s) {
-        int curr = 0, total = 0;
-        while(curr < s.size()) {
-            if(s[curr] == '1') total++;
-            curr++;
-        }
-        return total;
-    }
-
-    int totalOneFromRight(string& s) {
-        int curr = s.size() - 1, total = 0;
-        while(curr >= 0) {
-            if(s[curr] != '1') break;
-            total++; curr--;
-        }
-        return total;
-    }
-
     int maxOperations(string s) {
-        int curr = 0, ans = 0, bundle = 0, total = totalOne(s), totalFromRight = totalOneFromRight(s);
-        curr = findNextOne(s, curr);
-        total -= totalFromRight;
-
-        while(bundle != total && curr != -1) {
-            int  currBundle = findTotalBundledOne(s, curr), next = findNextOne(s, curr + currBundle);
-            bundle += currBundle;
-            ans += bundle;
-            curr = next;
+        int ans = 0, one = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') one++;
+            else if(s[i] == '0' && i > 0 && s[i - 1] == '1') ans += one;
         }
-
         return ans;
     }
 };
