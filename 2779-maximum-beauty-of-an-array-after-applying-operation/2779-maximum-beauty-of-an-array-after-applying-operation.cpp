e6a592ababe1b33054e34d42e65b5166a7a89f7b
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-13 15:42:32 +0700

    Time: 1322 ms (5.19%), Space: 306.5 MB (5.19%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        vector<int> diff(1000000 + 2, 0);
+        for(auto &x : nums) {
+            int start = x - k, end = x + k;
+            if(start < 0) start = 0;
+            diff[start]++; diff[end + 1]--;
+        }
+
+        int ans = 1, curr = 0;
+        for(auto& x : diff) {
+            curr += x;
+            ans = max(ans, curr);
+        }
+
+        return ans;
+    }
+};
