428223b5347a8e8ee02cc6521598bc7daf5afead
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-13 19:32:29 +0700

    Time: 3 ms (8.88%), Space: 32.2 MB (12.2%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        if(grid.size() == 3) return false;
+        vector<int> x = {-1, -2, -1, -2, 1, 2, 1, 2};
+        vector<int> y = {2, 1, -2, -1, -2, -1, 2, 1};
+
+        int currX = 0, currY = 0, next = 1, n = grid.size(), gridSize = pow(n, 2);
+        while(next != gridSize - 1) {
+            bool flag = false;
+            for(int i = 0; i < 8; i++) {
+                int nextX = currX + x[i], nextY = currY + y[i];
+
+                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= n) continue;
+                if(grid[nextX][nextY] == next) {
+                    flag = true;
+                    currX = nextX; currY = nextY;
+                    next++;
+                    break;
+                }
+            }
+
+            if(!flag) return false;
+        }
+        cout << next << " " << currX << " " << currY << endl;
+        
+        return true;
+    }
+};
