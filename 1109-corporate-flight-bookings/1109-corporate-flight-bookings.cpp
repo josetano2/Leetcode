7d8fd294591d367f965885752de39e8b3593fcef
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-13 14:43:12 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> ans(n, 0);
+        vector<int> diff(n + 2, 0);
+        vector<int> ans;
         for(int i = 0; i < bookings.size(); i++) {
             int first = bookings[i][0], last =  bookings[i][1], seats = bookings[i][2];
-            for(int j = first - 1; j < last; j++) ans[j] += seats;
+            diff[first] += seats; diff[last + 1] -= seats;
         }
 
+        int curr = 0;
+        for(int i = 1; i <= n; i++) {
+            curr += diff[i];
+            ans.push_back(curr);
+        }
         return ans;
     }
 };
