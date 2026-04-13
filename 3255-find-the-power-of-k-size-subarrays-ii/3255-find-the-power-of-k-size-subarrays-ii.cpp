19c359626e6436770a4b36b902a392b5383d6e59
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-13 19:08:28 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
+++ 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         vector<int> ans(nums.size() - k + 1, -1);
         int l = 0, r = 1;
-
         if(k == 1) {
             while(l < nums.size()) {
                 ans[l] = nums[l]; l++;
             }
         }
-
         else {
             while (r < nums.size()) {
                 if (nums[r] - 1 != nums[r - 1]) l = r;
@@ -20,7 +18,6 @@ public:
                 r++;
             }
         }
-        
         return ans;
     }
 };
