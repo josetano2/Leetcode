214433b6ef45b9099d9aa45f0caf76779418259b
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-15 19:25:16 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -10,51 +10,22 @@ public:
         while(!q.empty()) {
             auto [l, r] = q.front();
             q.pop();
-            if(l > x || r > y) continue;
+
             if(l + r == target) return true;
 
             if(visited[l][r]) continue;
             visited[l][r] = true;
 
-            if(l == 0) {
-                q.push({l + x, r});
-            }
-            if(r == 0) {
-                q.push({l, r + y});
-            }
-
-            if(l == x) {
-                q.push({0, r});
-            }
-
-            if(r == y) {
-                q.push({l, 0});
-            }
-
-            // spill left to right
-            if(r < y) {
-                int newL = l, newR = r, rem = 0;
-                newR = l + r;
-                if(newR > y) {
-                    rem = abs(y - newR);
-                    newR = y;
-                }
-                newL = abs(l - newR);
-                q.push({newL, newR});
-            }
-
-            // spill right to left
-            if(l < x) {
-                int newL = l, newR = r, rem = 0;
-                newL = l + r;
-                if(newL > x) {
-                    rem = abs(x - newL);
-                    newL = x;
-                }
-                newR = abs(r - newL);
-                q.push({newL, newR});
-            }
+            if(l == 0) q.push({x, r});
+            if(r == 0) q.push({l, y});
+            if(l == x) q.push({0, r});
+            if(r == y) q.push({l, 0});
             
+            int rem = min(l, y - r);
+            q.push({l - rem, r + rem});
+
+            rem = min(x - l, r);
+            q.push({l + rem, r - rem});
         }
 
         return false;
