40c8fb22037947ddda92f44bcf0d5abe1660bc83
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-16 19:05:34 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,15 +3,13 @@ public:
 
     void backtrack(string curr, int idx, vector<char>& abc, vector<string>& happy, int n) {
         if(curr.size() == n) {
-            // cout << "inserted" << endl << endl;
             happy.push_back(curr);
             return;
         }
-        // cout << curr << " " << idx << endl;
+        
         for(int i = 0; i < abc.size(); i++) {
             if(idx > 0 && curr[idx - 1] == abc[i]) continue;
             curr += abc[i];
-            // cout << curr << " " << i << endl;
             backtrack(curr, idx + 1, abc, happy, n);
             curr.pop_back(); 
         }
