b9949ee619263be96fa3531c45624bddf7a93e5b
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-17 16:48:20 +0700

    Time: 95 ms (5.44%), Space: 60 MB (11.46%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int l = 1, r = INT_MIN, mid = 0;
+        for(int i = 0; i < nums.size(); i++) r = max(r, nums[i]);
+
+        while(l <= r) {
+            int curr = 0;
+            mid = l + (r - l) / 2;
+
+            bool flag = true;
+            for(auto& x : nums) {
+                if(x <= mid) continue;
+
+                curr = curr + (ceil((double) x / mid) - 1);
+                if(curr > maxOperations) {
+                    flag = false; break;
+                }
+            }
+
+            if(flag) r = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
