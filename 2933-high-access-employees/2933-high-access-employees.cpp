0c260355805ca57e5b93fbcae9dd8031124c6927
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-17 20:17:59 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2933-high-access-employees/2933-high-access-employees.cpp
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -7,10 +7,8 @@ public:
     vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
         
         unordered_map<string, vector<string>> um;
-        for(auto& x : access_times) {
-            um[x[0]].push_back(x[1]);
-        }
-        
+        for(auto& x : access_times) um[x[0]].push_back(x[1]);
+
         vector<string> ans;
         for(auto& [x, y] : um) {
             if(y.size() < 3) continue;
@@ -18,22 +16,18 @@ public:
 
             int l = 0, r = 1, mm = 0, ss = 0;
             while(r < y.size()) {
-                mm = timeConvert(y[l][0], y[l][1]), ss = timeConvert(y[l][2], y[l][3]);
                 int currM = timeConvert(y[r][0], y[r][1]), currS = timeConvert(y[r][2], y[r][3]);
-                if(currM >= mm && currM <= mm + 1 && ((currM == mm && currS >= ss) || (currM == mm + 1 && currS <= ss - 1))) {
-                    r++;
-                }
+                mm = timeConvert(y[l][0], y[l][1]), ss = timeConvert(y[l][2], y[l][3]);
+                if(currM >= mm && currM <= mm + 1 && ((currM == mm && currS >= ss) || (currM == mm + 1 && currS <= ss - 1))) r++;
                 else {
                     l++; r = l + 1;
                 }
-
                 if(r - l == 3) {
-                    ans.push_back(x);
+                    ans.push_back(x); 
                     break;
                 }
             }
         }
-
 
         return ans;
     }
