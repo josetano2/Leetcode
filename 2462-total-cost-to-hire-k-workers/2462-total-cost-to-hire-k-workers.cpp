7621b284db17a50fb703d1eaa63d3f64e5c8b1c2
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-17 18:10:50 +0700

    Time: 67 ms (11.55%), Space: 88.6 MB (5.09%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pqL, pqR;
+        int l = 0, r = costs.size();
+        for(int i = 0; i < candidates; i++){
+            pqL.push({costs[i], i});
+            l++;
+        }
+
+        for(int i = costs.size() - 1; i >= costs.size() - candidates; i--) {
+            if(l >= r) break;
+            pqR.push({costs[i], i});
+            r--;
+        }
+        l--;
+
+        long long ans = 0;
+        while(k > 0) {
+            if(!pqL.empty() && !pqR.empty()) {
+                auto topL = pqL.top(), topR = pqR.top();
+                if(topL.first < topR.first) {
+                    ans += topL.first;
+                    pqL.pop();
+
+                    l++;
+                    if(l < r) pqL.push({costs[l], l});
+                }
+                else if(topL.first > topR.first) {
+                    ans += topR.first;
+                    pqR.pop();
+                    
+                    r--;
+                    if(l < r) pqR.push({costs[r], r});
+                }
+                else {
+                    if(topL.second < topR.second) {
+                        ans += topL.first;
+                        pqL.pop();
+
+                        l++;
+                        if(l < r) pqL.push({costs[l], l});
+                    }
+                    else {
+                        ans += topR.first;
+                        pqR.pop();
+                        
+                        r--;
+                        if(l < r) pqR.push({costs[r], r});
+                    }
+                }
+            }
+
+            else {
+                auto topL = pqL.top();
+                ans += topL.first;
+                pqL.pop();
+            }
+
+            if(r - l <= 1) {
+                while(!pqR.empty()) {
+                    auto topR = pqR.top();
+                    pqR.pop();
+                    pqL.push(topR);
+                }
+            }
+
+            k--;
+        }
+
+        return ans;
+    }
+};
