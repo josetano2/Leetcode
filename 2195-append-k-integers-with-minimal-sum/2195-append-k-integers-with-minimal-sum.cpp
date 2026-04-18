2dc569af5bc65a43dbd7fbd50d67a1d626dd6903
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-18 19:35:56 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    long long getMax(int a, int b) {
-        return a > b ? a : b;
-    }
-
     long long arit(long long n) {
         return n * (n + 1) / 2;
     }
@@ -12,20 +8,20 @@ public:
 
         long long curr = 0, ans = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(k <= 0) return ans;
             if(curr == nums[i] || nums[i] - curr == 1) {
                 curr = nums[i];
                 continue;
             }
 
-            long long diff = nums[i] - curr - 1, l = 0, r = 0;
-            if(k - diff >= 0) l = arit(curr), r = arit(nums[i] - 1);
-            else l = arit(curr), r = arit(curr + k);
-            cout << curr << " " << nums[i] << " " << l << " " << r << endl;
+            long long diff = nums[i] - curr - 1, l = arit(curr), r = 0;
+            if(k - diff >= 0) r = arit(nums[i] - 1);
+            else r = arit(curr + k);
 
             ans += (r - l);
             k -= diff;
             curr = nums[i];
+
+            if(k <= 0) return ans;
         }
 
         if(k > 0) {
