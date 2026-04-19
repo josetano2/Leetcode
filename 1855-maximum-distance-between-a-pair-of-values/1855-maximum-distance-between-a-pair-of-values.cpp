3f2abd012ac2ef34a3efa070b90f0c4bce85c35b
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-19 16:52:50 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -4,16 +4,10 @@ public:
         int ans = INT_MIN;
         for(int i = 0; i < nums1.size(); i++) {
             int l = i, r = nums2.size() - 1, mid = 0, curr = nums1[i];
-
             while(l <= r) {
                 mid = l + (r - l) / 2;
-
-                if(curr <= nums2[mid]) {
-                    l = mid + 1;
-                }
-                else {
-                    r = mid - 1;
-                }
+                if(curr <= nums2[mid]) l = mid + 1;
+                else r = mid - 1
             }
             if(i <= r) ans = max(ans, r - i);
         }
