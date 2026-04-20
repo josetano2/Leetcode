f712f4e1e988dd3c3b21ee23b54a475d1d6b4180
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-20 14:11:28 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,18 +4,14 @@ public:
         int l = 0, r = colors.size() - 1;
 
         while(l < r) {
-            if(colors[l] != colors[r]) {
-                return abs(r - l);
-            }
-
+            if(colors[l] != colors[r]) return abs(r - l);
+            
             int tL = l, tR = r;
             while(tL < tR && colors[l] == colors[tL]) tL++;
             while(tL < tR && colors[r] == colors[tR]) tR--;
-
+            
             if(r - tR > tL - l) l = tL;
             else r = tR;
-            // cout << tL - l << " " << r - tR << endl;
-            // r--;
         }
         
         return -1;
