387d862aa4d0a2656c0f35a0c66c64195eaff9b0
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-20 16:00:27 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // if(k > nums.size()) return;
-
-        k = k % nums.size();
-        cout << k << endl;
+        k %= nums.size();
         vector<int> temp;
 
-        for(int i = nums.size() - k; i < nums.size(); i++) {
-            temp.push_back(nums[i]);
-        }
-
-        for(int i = 0; i < nums.size() - k; i++) {
-            temp.push_back(nums[i]);
-
-        }
-
+        for(int i = nums.size() - k; i < nums.size(); i++) temp.push_back(nums[i]);
+        for(int i = 0; i < nums.size() - k; i++) temp.push_back(nums[i]);
         nums = temp;
     }
 };
