4d4d4bc21de9f2a058b5c361e1c5af61749b80eb
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-20 20:36:21 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
 
-    int cc(vector<int>& dp, vector<int>& coins, int curr) {
-        if(curr < 0) return INT_MAX;
-        if(curr == 0) return 0;
-
-        if(dp[curr] != -1) return dp[curr];
-
-        int next = INT_MAX;
-        for(int i = 0; i < coins.size(); i++) {
-            int temp = cc(dp, coins, curr - coins[i]);
-            if(temp != INT_MAX) next = min(temp + 1, next);
-        }
-
-        return dp[curr] = next;
-    } 
-
+    // bottom-up
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, -1);
-        int ans = cc(dp, coins, amount);
-        return ans == INT_MAX ? -1 : ans;
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            int s = amount + 1;
+            for(auto& c : coins) {
+                if(i - c >= 0) s = min(s, dp[i - c]);
+            }
+            if(s != amount + 1) dp[i] = s + 1;
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
+
+    // top-down
+    // int cc(vector<int>& dp, vector<int>& coins, int curr) {
+    //     if(curr < 0) return INT_MAX;
+    //     if(curr == 0) return 0;
+
+    //     if(dp[curr] != -1) return dp[curr];
+
+    //     int next = INT_MAX;
+    //     for(int i = 0; i < coins.size(); i++) {
+    //         int temp = cc(dp, coins, curr - coins[i]);
+    //         if(temp != INT_MAX) next = min(temp + 1, next);
+    //     }
+
+    //     return dp[curr] = next;
+    // } 
+
+    // int coinChange(vector<int>& coins, int amount) {
+    //     vector<int> dp(amount + 1, -1);
+    //     int ans = cc(dp, coins, amount);
+    //     return ans == INT_MAX ? -1 : ans;
+    // }
 };
