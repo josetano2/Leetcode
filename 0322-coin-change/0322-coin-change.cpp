9794ca025da33f8b3a8a23fc0a209befac97dbd3
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-20 20:27:38 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,7 +5,7 @@ public:
         if(curr < 0) return INT_MAX;
         if(curr == 0) return 0;
 
-        if(dp[curr] != INT_MAX) return dp[curr];
+        if(dp[curr] != -1) return dp[curr];
 
         int next = INT_MAX;
         for(int i = 0; i < coins.size(); i++) {
@@ -17,7 +17,7 @@ public:
     } 
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount + 1, -1);
         int ans = cc(dp, coins, amount);
         return ans == INT_MAX ? -1 : ans;
     }
