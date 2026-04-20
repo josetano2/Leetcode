4131a08969416ca2435a82d8cc55b3faffb375c4
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-20 20:25:55 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
 
     int cc(vector<int>& dp, vector<int>& coins, int curr) {
+        if(curr < 0) return INT_MAX;
         if(curr == 0) return 0;
-        if(curr < 0) return INT_MAX - 1;
 
-        if(dp[curr] != INT_MAX - 1) return dp[curr];
+        if(dp[curr] != INT_MAX) return dp[curr];
 
-        int next = INT_MAX - 1;
+        int next = INT_MAX;
         for(int i = 0; i < coins.size(); i++) {
             int temp = cc(dp, coins, curr - coins[i]);
-            next = min(temp, next);
+            if(temp != INT_MAX) next = min(temp + 1, next);
         }
 
-        return dp[curr] = next + 1;
+        return dp[curr] = next;
     } 
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX - 1);
+        vector<int> dp(amount + 1, INT_MAX);
         int ans = cc(dp, coins, amount);
         return ans == INT_MAX ? -1 : ans;
     }
