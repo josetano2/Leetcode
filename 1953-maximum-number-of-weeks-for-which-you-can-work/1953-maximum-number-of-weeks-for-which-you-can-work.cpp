ebe8ac60094953588fb3d5c9b0cd1cc682a061f0
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-21 17:53:58 +0700

    Time: 1 ms (32.16%), Space: 79.3 MB (95.32%) - LeetHub

--- 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -4,8 +4,13 @@ public:
         int biggest = INT_MIN;
         for(auto& x : milestones) biggest = max(x, biggest);
         
-        long long total = 0;
-        for(int i = 0; i < milestones.size(); i++) if(milestones[i] != biggest) total += milestones[i];;
+        long long total = 0, flag = false;
+        for(int i = 0; i < milestones.size(); i++) {
+            if(milestones[i] == biggest && !flag) {
+                flag = true; continue;
+            }
+            total += milestones[i];
+        }
     
         if(total < biggest) total = total * 2 + 1;
         else total += biggest;
