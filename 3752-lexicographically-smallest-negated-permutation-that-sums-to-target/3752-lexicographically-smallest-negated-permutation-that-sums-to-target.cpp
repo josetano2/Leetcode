ffea8e3cdaf495f99bd1b934d881e4d370b450c0
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-22 18:05:49 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
@@ -5,6 +5,7 @@ public:
         long long currPos = (long long) n * (n + 1) / 2, currNeg = 0;
 
         unordered_set<int> us;
+        vector<int> cache(n + 1, 0);
         long long r = n * -1;
         if(currPos < target) return vector<int>(0);
 
@@ -18,14 +19,14 @@ public:
                 currPos += res;
 
                 ans.push_back(res);
-                us.insert(res);
+                cache[abs(res)] = 1;
                 break;
             }
             else {
                 currNeg += r;
                 currPos += r;
                 ans.push_back(r);
-                us.insert(r);
+                cache[abs(r)] = 1;
 
                 r++;
             } 
@@ -33,7 +34,7 @@ public:
         
         if(currPos + currNeg != target) return vector<int>(0);
          
-        for(int i = 1; i <= n; i++) if(us.find(i * -1) == us.end()) ans.push_back(i);
+        for(int i = 1; i <= n; i++) if(cache[i] == 0) ans.push_back(i);
         
         return ans;
     }
