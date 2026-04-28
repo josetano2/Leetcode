23e080006043de1f963716ddf9d022055c9efe51
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-04-28 20:43:11 +0700

    Time: 54 ms (16.05%), Space: 90.4 MB (9.47%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+
+        int m = grid.size(), n = grid[0].size();
+
+        unordered_map<int, int> um;
+        vector<int> nums;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                um[grid[i][j] % x]++;
+                nums.push_back(grid[i][j]);
+            }
+        }
+        
+        for(auto& [x, y] : um) {
+            cout << x << " " << y << endl;
+        }
+
+        if(um.size() > 1) return -1;
+
+        sort(nums.begin(), nums.end());
+        int target = (nums.size() - 1) / 2, ans = 0;
+        cout << target << endl;
+        for(int i = 0; i < nums.size(); i++) {
+            int diff = (abs(nums[target] - nums[i])) / x;
+            ans += diff;
+        }
+
+        return ans;
+    }
+};
