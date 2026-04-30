867ce5a11f3512764488e162624c405991a2698e
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-01 03:31:43 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-
     int ans(int x, int y, int c, int m, int n, vector<vector<vector<int>>>& dp, vector<vector<int>>& grid) {
-        if(x >= m || y >= n) return INT_MIN;
-        if(grid[x][y] != 0 && c <= 0) return INT_MIN;
-        if(x == m - 1 && y == n - 1) return grid[x][y];
+        if (x >= m || y >= n) return INT_MIN;
+        if (grid[x][y] != 0 && c <= 0) return INT_MIN;
+        if (x == m - 1 && y == n - 1) return grid[x][y];
 
-        if(dp[x][y][c] != INT_MIN + 1) return dp[x][y][c];
+        if (dp[x][y][c] != INT_MIN + 1) return dp[x][y][c];
 
         int cost = grid[x][y] == 0 ? 0 : 1;
-        dp[x][y][c] = max(ans(x + 1, y, c - cost, m, n, dp, grid) + grid[x][y], ans(x, y + 1, c - cost, m, n, dp, grid) + grid[x][y]);
+        int down = ans(x + 1, y, c - cost, m, n, dp, grid);
+        int right = ans(x, y + 1, c - cost, m, n, dp, grid);
+
+        int best = INT_MIN;
+        if (down != INT_MIN) best = max(best, down + grid[x][y]);
+        if (right != INT_MIN) best = max(best, right + grid[x][y]);
+
+        dp[x][y][c] = best;
         return dp[x][y][c];
     }
 
