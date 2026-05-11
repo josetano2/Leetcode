a415117cc5d1a8e42598d661ab9e9013b33c0aeb
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-11 19:29:25 +0700

    Time: 0 ms (100%), Space: 10.7 MB (46.98%) - LeetHub

---
+++ 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+
+    bool hasWon(vector<string>& board, char c) {
+        string winningPiece = "";
+        winningPiece.append(3, c);
+
+        for(int i = 0; i < 3; i++) {
+            if(board[i] == winningPiece) return true;
+        }
+
+        for(int j = 0; j < 3; j++) {
+            string s = "";
+            s += board[0][j];
+            s += board[1][j];
+            s += board[2][j];
+            if(s == winningPiece) return true;
+        }
+
+        // diagonal
+        string s = "";
+        s += board[0][0];
+        s += board[1][1];
+        s += board[2][2];
+        if(s == winningPiece) return true;
+
+        s = "";
+        s += board[2][0];
+        s += board[1][1];
+        s += board[0][2];        
+        if(s == winningPiece) return true;
+
+        return false;
+    }
+
+    bool validTicTacToe(vector<string>& board) {
+        int x = 0, o = 0;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(board[i][j] == 'X') x++;
+                if(board[i][j] == 'O') o++;
+            }
+        }
+        
+        bool xWon = hasWon(board, 'X');
+        bool oWon = hasWon(board, 'O');
+
+        if(xWon && oWon) return false;
+        if(xWon) {
+            cout << x << " " << o << endl;;
+            if(x == o + 1) return true;
+            else return false;
+        }
+
+        if(oWon) {
+            if(x == o) return true;
+            else return false;
+        }
+
+        if(x == 0 && o == 1) return false;
+        if(x == o || x == o + 1) return true;
+        return false;
+    }
+};
