9025e3abe8015ee8ee9c6dad39c6b890dd0d6830
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-11 18:57:24 +0700

    Time: 321 ms (39.74%), Space: 149.9 MB (43.16%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -7,21 +7,17 @@ public:
 
         for(int i = 0; i < n; i++) um[arr[i]] = i;
 
+        int ans = INT_MIN;
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
                 int target = arr[i] - arr[j];
                 if(um.find(target) != um.end() && um[target] < j) {
                     dp[j][i] = max(dp[j][i], dp[um[target]][j] + 1);
+                    ans = max(dp[j][i], ans);
                 }
             }
         }
 
-        int ans = INT_MIN;
-        for (const auto& row : dp) {
-            if (!row.empty()) {
-                ans = max(ans, *max_element(row.begin(), row.end()));
-            }
-        }
         return ans < 3 ? 0 : ans;
     }
 };
