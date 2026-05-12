ba601162b25d6477b30a9fb798771781460fca4a
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-12 12:15:43 +0700

    Time: 59 ms (40.29%), Space: 97.5 MB (39.81%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](const auto& a, const auto& b) {
+            return a[1] - a[0] > b[1] - b[0];
+        });
+
+        int min = 0, max = 0;
+        for(auto& t : tasks) {
+            min += t[0]; max += t[1];
+        }
+
+        while(min < max) {
+            int mid = min + (max - min) / 2;
+            bool isValid = true;
+            int temp = mid;
+            for(auto& t : tasks) {
+                if(temp >= t[1]) {
+                    temp -= t[0];
+                }
+                else {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if(isValid) {
+                max = mid;
+            }
+            else {
+                min = mid + 1;
+            }
+        }
+        return min;
+    }
+};
