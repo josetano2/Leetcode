24f3e2b614f785717690210b24d3ea03f58072b9
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-12 19:01:14 +0700

    Time: 0 ms (100%), Space: 61.7 MB (33.78%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int getMaxLen(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(2, INT_MIN));
+        int prevPos = 0, prevNeg = 0, currPos = 0, currNeg = 0;
 
-        
         if(nums[0] > 0){
-            dp[0][0] = 1;
-            dp[0][1] = 0;
+            prevPos = 1;
+            prevNeg = 0;
         }
         else if(nums[0] < 0) {
-            dp[0][0] = 0;
-            dp[0][1] = 1;
+            prevPos = 0;
+            prevNeg = 1;
         }
         else {
-            dp[0][0] = 0;
-            dp[0][1] = 0;
+            prevPos = 0;
+            prevNeg = 0;
         }
 
 
-        int ans = dp[0][0];
+        int ans = prevPos;
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] > 0) {
-                dp[i][0] = dp[i - 1][0] + 1;
-                dp[i][1] = dp[i - 1][1] > 0 ? dp[i - 1][1] + 1 : 0;
+                currPos = prevPos + 1;
+                currNeg = prevNeg > 0 ? prevNeg + 1 : 0;
             }
             else if(nums[i] < 0) {
-                dp[i][1] = dp[i - 1][0] + 1;
-                dp[i][0] = dp[i - 1][1] > 0 ? dp[i - 1][1] + 1 : 0;
+                currNeg = prevPos + 1;
+                currPos = prevNeg > 0 ? prevNeg + 1 : 0;
             }
             else {
-                dp[i][0] = 0;
-                dp[i][1] = 0;
+                currPos = 0;
+                currNeg = 0;
             }
-            ans = max(ans, dp[i][0]);
+            ans = max(ans, currPos);
+            prevPos = currPos;
+            prevNeg = currNeg;
             
         }
         return ans;
