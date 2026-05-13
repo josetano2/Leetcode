e437c0bb4598286759c7e9f15bb615f2633a643c
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-13 18:34:17 +0700

    Time: 31 ms (33.84%), Space: 24.6 MB (60.46%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int dp(int idx, int free, vector<vector<int>>& cache, vector<int>& prices) {
+    int dp(int idx, vector<int>& cache, vector<int>& prices) {
         if(idx >= prices.size()) return 0;
+        if(cache[idx] != INT_MAX) return cache[idx];
 
-        if(free == -1) return cache[idx][0] = dp(idx + 1, 2 * idx + 1, cache, prices) + prices[idx];
-        if(cache[idx][free] != INT_MAX) return cache[idx][free];
-
-        int freeFruit = -1, paidFruit = -1;
-        if(idx <= free) freeFruit = dp(idx + 1, free, cache, prices);
-        paidFruit = dp(idx + 1, 2 * idx + 1, cache, prices) + prices[idx];
-
-        if(freeFruit == -1) cache[idx][free] = paidFruit;
-        else cache[idx][free] = min(freeFruit, paidFruit);
-        
-        return cache[idx][free];
+        int temp = INT_MAX;
+        for(int j = idx + 1; j <= 2 * idx + 2; j++) temp = min(temp, dp(j, cache, prices));
+        return cache[idx] = temp + prices[idx];
     }
 
     int minimumCoins(vector<int>& prices) {
-        vector<vector<int>> cache(prices.size(), vector<int>(prices.size() * 2, INT_MAX));
-        return dp(0, -1, cache, prices);
+        vector<int> cache(prices.size(), INT_MAX);
+        return dp(0, cache, prices);
     }
 };
