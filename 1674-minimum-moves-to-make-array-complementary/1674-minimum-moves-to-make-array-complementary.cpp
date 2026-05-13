58ae04da993f0085c6e6c7881c51b4a5d811e6be
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-13 16:46:16 +0700

    Time: 16 ms (40.71%), Space: 93.5 MB (43.36%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> prefixSum(limit * 2 + 2, 0);
         int n = nums.size();
         for(int i = 0; i < n / 2; i++) {
-            int a = min(nums[i], nums[n - i - 1]), b = max(nums[i], nums[n - i - 1]), diff = a + b;
+            int a = min(nums[i], nums[n - i - 1]), b = max(nums[i], nums[n - i - 1]);
                 prefixSum[2] += 2;
                 prefixSum[a + 1] -= 2;
             
