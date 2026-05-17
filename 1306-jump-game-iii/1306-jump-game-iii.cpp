a7941ccf1b4cc634aeae8435966bbc1e6f929f4d
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-17 18:58:11 +0700

    Time: 1 ms (54.71%), Space: 39.6 MB (35.05%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    void dfs(int idx, bool& flag, vector<int>& arr, vector<bool>& isVisited) {
+        if(idx < 0 || idx >= arr.size() || isVisited[idx]) return;
 
-    bool flag = false;
-    vector<bool> isVisited;
-    void dfs(int idx, vector<int>& arr) {
-        if(idx < 0 || idx > arr.size() || isVisited[idx]) return;
         if(arr[idx] == 0) {
             flag = true;
             return;
         }
         
         isVisited[idx] = true;
-        dfs(idx + arr[idx], arr);
-        dfs(idx - arr[idx], arr);
+        dfs(idx + arr[idx], flag, arr, isVisited);
+        dfs(idx - arr[idx], flag, arr, isVisited);
     }
 
     bool canReach(vector<int>& arr, int start) {
-        isVisited = vector<bool>(arr.size(), false);
-        dfs(start, arr);
+        bool flag = false;
+        vector<bool> isVisited = vector<bool>(arr.size(), false);
+        dfs(start, flag, arr, isVisited);
         return flag;
     }
 };
