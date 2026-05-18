9a7ccd9e200a33b19a90b554d2a0dd5bb8413aeb
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-18 16:08:33 +0700

    Time: 128 ms (38.9%), Space: 145.5 MB (5.03%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -13,16 +13,15 @@ public:
         visited[0] = true;
 
         int level = 0;
-        cout << arr.size() << endl;;
         while(!q.empty()) {
             int s = q.size();
             for(int i = 0; i < s; i++) {
                 auto [idx, val] = q.front();
                 q.pop();
-                
+
                 if(idx == arr.size() - 1) return level;
 
-                if(um.count(arr[idx])) {
+                if(um.find(arr[idx]) != um.end() && um[arr[idx]].size() > 0) {
                     queue<int>& qt = um[arr[idx]];
                     while(!qt.empty()) {
                         int f = qt.front();
@@ -35,20 +34,16 @@ public:
                             q.push({f, arr[f]});
                         }
                     }
-
-                    um.erase(arr[idx]);
                 }
 
                 if(idx + 1 < arr.size() && !visited[idx + 1]) {
                     visited[idx + 1] = true;
                     q.push({idx + 1, arr[idx + 1]});
-                    // if(um.find(arr[idx + 1]) != um.end() && um[arr[idx + 1]].size() > 0) um[arr[idx + 1]].pop();
                 }
 
                 if(idx - 1 >= 0 && !visited[idx - 1]) {
                     visited[idx - 1] = true;
                     q.push({idx - 1, arr[idx - 1]});
-                    // if(um.find(arr[idx - 1]) != um.end() && um[arr[idx - 1]].size() > 0) um[arr[idx - 1]].pop();
                 }
             }
             level++;
