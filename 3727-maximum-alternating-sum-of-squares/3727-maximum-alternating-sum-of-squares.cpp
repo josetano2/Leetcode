e71835f04d7cff9483cd48f2783d9af09c91c5cc
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-18 18:38:37 +0700

    Time: 69 ms (30.31%), Space: 176.1 MB (47.49%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.cpp
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) {
         for(int i = 0; i < nums.size(); i++) nums[i] = abs(nums[i]);
-        sort(nums.begin(), nums.end());
-        
-        int l = 0, r = nums.size() - 1;
-        long long ans = 0;
-        while(l <= r) {
-            ans += pow(nums[r], 2);
-            r--;
+        sort(nums.begin(), nums.end(), greater<int>());
 
-            if(l <= r) {
-                ans -= pow(nums[l], 2);
-                l++;
-            }
-        }
+        long long ans = 0;
+        int limit = nums.size() / 2.0;
+        if(nums.size() % 2 == 0) limit--;
+
+        for(int i = 0; i <= limit; i++) ans += pow(nums[i], 2);
+        for(int i = limit + 1; i < nums.size(); i++) ans -= pow(nums[i], 2);
+        
         return ans;
     }
 };
