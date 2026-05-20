c1aa49a5c9ace7f50c3f8dce9e9e3d37172279fb
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-20 17:16:04 +0700

    Time: 46 ms (18.3%), Space: 105.2 MB (16.03%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -3,11 +3,18 @@ public:
     int minRemoval(vector<int>& nums, long long k) {
         sort(nums.begin(), nums.end());
 
-        int n = nums.size(), ans = INT_MAX;
+        int n = nums.size();
+        int j = 0;
+        int longest = 0;
+
         for(int i = 0; i < n; i++) {
-            int j = upper_bound(nums.begin(), nums.end(), (long long) nums[i] * k) - nums.begin() - 1;
-            ans = min(ans, n - (j - i) - 1);
+            while(j < n && (long long) nums[i] * k >= nums[j]) {
+                j++;
+            }
+
+            longest = max(longest, j - i);
         }
-        return ans;
+
+        return n - longest;
     }
 };
