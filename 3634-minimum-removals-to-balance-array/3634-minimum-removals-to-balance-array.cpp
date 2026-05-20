939ff7a1474b875b29af8fea17b5bd61a77abd6b
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-20 17:16:39 +0700

    Time: 67 ms (7.66%), Space: 105 MB (84.93%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int minRemoval(vector<int>& nums, long long k) {
+    int minRemoval(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
 
-        int n = nums.size();
-        int j = 0;
-        int longest = 0;
-
+        int n = nums.size(), ans = INT_MAX;
         for(int i = 0; i < n; i++) {
-            while(j < n && (long long) nums[i] * k >= nums[j]) {
-                j++;
-            }
-
-            longest = max(longest, j - i);
+            int j = upper_bound(nums.begin(), nums.end(), (long long) nums[i] * k) - nums.begin() - 1;
+            ans = min(ans, n - (j - i) - 1);
         }
-
-        return n - longest;
+        return ans;
     }
 };
