6a34a93fd8c9281094a4a568c1c6bcaa90584d3f
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-20 18:05:26 +0700

    Time: 123 ms (5.14%), Space: 28.6 MB (68.22%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -6,17 +6,15 @@ public:
 
         for(int i = 0; i < n; i++) {
             long long c = heights[i];
-            vector<int> h = heights;
-
             int lm = INT_MAX;
             for(int j = i - 1; j >= 0; j--) {
-                lm = min({heights[i], h[j], lm});
+                lm = min({heights[i], heights[j], lm});
                 c += lm;
             }
             
             int rm = INT_MAX;
             for(int j = i + 1; j < n; j++) {
-                rm = min({heights[i], h[j], rm});
+                rm = min({heights[i], heights[j], rm});
                 c += rm;
             }
             ans = max(c, ans);
