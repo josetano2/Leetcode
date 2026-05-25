9132a7690decaff9cdfb6a50ed978152904fffa5
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-05-25 19:08:12 +0700

    Time: 8 ms (71.26%), Space: 29.1 MB (28.05%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        
+        vector<int> visited(s.size(), false);
+        visited[0] = true;
+        int n = s.size(), count = 0;
+
+        for(int i = minJump; i < n; i++) {
+            
+            if(visited[i - minJump]) {
+                count++;
+            }
+
+            if(i - maxJump - 1 >= 0 && visited[i - maxJump - 1]) {
+                count--;
+            }
+            
+            if(count > 0 && s[i] == '0') {
+                visited[i] = true;
+            }
+        }
+
+        return visited[n - 1];
+    }
+};
