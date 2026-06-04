939af7b9da77eff6121c3f2dd2e507416713eb17
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-04 18:54:53 +0700

    Time: 0 ms (100%), Space: 9.4 MB (25.73%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int minCost(int n) {
-        vector<int> dp(n + 1, -1);
-
-        // dp[2] = 1;
-        // dp[3] = 2;
 
         int cost = 0;
         queue<int> q;
+        
         q.push(n / 2);
         if(n % 2 == 0) {
             q.push(n / 2);
@@ -22,13 +19,8 @@ public:
             int top = q.front();
             q.pop();
             
-            // if(dp[top] != -1) {
-            //     cost += dp[top]; continue;
-            // }
-
             int a = top / 2, b = top / 2;
             if(top % 2 != 0) b++;
-            cout << a << " " << b << " " << cost << endl;
 
             if(a > 1) q.push(a);
             if(b > 1) q.push(b);
