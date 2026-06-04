ed674d61fa3ba8b2c78065a6e11bb266374626f3
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-05 04:48:45 +0700

    Time: 0 ms (100%), Space: 16.4 MB (14.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-
-    bool isValid(int x, int y, int m, int n) {
-        if(x < 0 || x >= m || y < 0 || y >= n) return false;
-        return true;
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
 
@@ -13,11 +8,10 @@ public:
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(isValid(i, j - 1, m, n)) dp[i][j] = min(dp[i][j], grid[i][j] + dp[i][j - 1]);
-                if(isValid(i - 1, j, m, n)) dp[i][j] = min(dp[i][j], grid[i][j] + dp[i - 1][j]);
+                if(j - 1 >= 0) dp[i][j] = min(dp[i][j], grid[i][j] + dp[i][j - 1]);
+                if(i - 1 >= 0) dp[i][j] = min(dp[i][j], grid[i][j] + dp[i - 1][j]);
             }
         }
-
         return dp[m - 1][n - 1];
     }
 };
