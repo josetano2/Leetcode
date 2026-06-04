1fd8af6604817c5beedae4fb41900164db527e8a
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-05 04:49:42 +0700

    Time: 3 ms (31.55%), Space: 14.3 MB (37.86%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     using ll = long long;
-    bool isValid(int x, int y, int m, int n) {
-        if(x < 0 || x >= m || y < 0 || y >= n) return false;
-        return true;
-    }
-    
     int maxProductPath(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), MOD = 1e09 + 7;
         
@@ -18,18 +13,17 @@ public:
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 // candidates left and above
-                if(isValid(i, j - 1, m, n)) {
+                if(j - 1 >= 0) {
                     dpMin[i][j] = min({dpMin[i][j], dpMin[i][j - 1] * grid[i][j], dpMax[i][j - 1] * grid[i][j]});
                     dpMax[i][j] = max({dpMax[i][j], dpMin[i][j - 1] * grid[i][j], dpMax[i][j - 1] * grid[i][j]});
                 }
 
-                if(isValid(i - 1, j, m, n)) {
+                if(i - 1 >= 0) {
                     dpMin[i][j] = min({dpMin[i][j], dpMin[i - 1][j] * grid[i][j], dpMax[i - 1][j] * grid[i][j]});
                     dpMax[i][j] = max({dpMax[i][j], dpMin[i - 1][j] * grid[i][j], dpMax[i - 1][j] * grid[i][j]});
                 }
             }
         }
-
         return dpMax[m - 1][n - 1] < 0 ? -1 : dpMax[m - 1][n - 1] % MOD;
     }
 };
