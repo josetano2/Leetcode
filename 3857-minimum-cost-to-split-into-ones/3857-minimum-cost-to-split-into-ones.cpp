bf0bf5eea130236b68139df9aef66e2986a873a3
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-04 18:56:29 +0700

    Time: 3 ms (25.92%), Space: 9.4 MB (25.86%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -4,30 +4,21 @@ public:
 
         int cost = 0;
         queue<int> q;
-        
-        q.push(n / 2);
-        if(n % 2 == 0) {
-            q.push(n / 2);
-            cost = (n / 2) * (n / 2);
-        }
-        else {
-            q.push(n / 2 + 1);
-            cost = (n / 2) * (n / 2 + 1);
-        }
+
+        q.push(n / 2); q.push(n - (n / 2));
+        cost = (n / 2) * (n - n / 2);
 
         while(!q.empty()) {
             int top = q.front();
             q.pop();
             
-            int a = top / 2, b = top / 2;
-            if(top % 2 != 0) b++;
+            int a = top / 2, b = top - top / 2;
 
             if(a > 1) q.push(a);
             if(b > 1) q.push(b);
 
             cost += (a * b);
         }
-
 
         return cost;
     }
