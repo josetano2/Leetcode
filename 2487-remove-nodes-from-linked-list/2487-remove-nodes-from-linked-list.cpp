ef147a6b2b4501cbeebfd9b484f5d01ddf74bbf8
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-05 17:07:49 +0700

    Time: 37 ms (14.71%), Space: 184.1 MB (16.62%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -4,16 +4,10 @@ public:
         if(head == nullptr) return nullptr;
 
         stack<int> stk;
-
         ListNode* curr = head;
         while(curr != nullptr) {
-            if(stk.empty()) {
-                stk.push(curr->val);
-            }
-            else {
-                while(!stk.empty() && curr->val > stk.top()) stk.pop();
-                stk.push(curr->val);
-            }
+            while(!stk.empty() && curr->val > stk.top()) stk.pop();
+            stk.push(curr->val);
             curr = curr->next;
         }
 
