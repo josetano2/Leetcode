b5015a028ccb6bbf52947bf6d5b003ef5b1e549a
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-05 17:05:23 +0700

    Time: 47 ms (6.91%), Space: 187.5 MB (10.69%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,18 +1,7 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
         if(head == nullptr) return nullptr;
-
 
         stack<int> stk;
 
@@ -22,17 +11,12 @@ public:
                 stk.push(curr->val);
             }
             else {
-                if(curr->val > stk.top()) {
-                    while(!stk.empty() && curr->val > stk.top()) {
-                        stk.pop();
-                    }
-                }
+                while(!stk.empty() && curr->val > stk.top()) stk.pop();
                 stk.push(curr->val);
             }
             curr = curr->next;
         }
 
-        stack<int> temp = stk;
         vector<int> rev;
         while (!stk.empty()) {
             rev.push_back(stk.top());
