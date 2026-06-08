0a428060569267596f1387f1e6abbc6ecaac2510
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-08 19:48:25 +0700

    Time: 501 ms (5.15%), Space: 70.4 MB (37.11%) - LeetHub

---
+++ 2841-maximum-sum-of-almost-unique-subarray/2841-maximum-sum-of-almost-unique-subarray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int m, int k) {
+        
+        long long ans = INT_MIN, sum = 0;
+
+        unordered_map<int, int> freq;
+        for(int i = 0; i < nums.size(); i++) {
+            if(i + 1 >= k) {
+                freq[nums[i]]++;
+                sum += nums[i];
+
+                // check total distinct
+                cout << freq.size() << " ";
+                if(freq.size() >= m) ans = max(sum, ans);
+            
+                freq[nums[i - k + 1]]--;
+                if(freq[nums[i - k + 1]] == 0) freq.erase(nums[i - k + 1]);
+                sum -= nums[i - k + 1];
+            }
+            else {
+                freq[nums[i]]++;
+                sum += nums[i];
+            }
+        }
+
+        return ans == INT_MIN ? 0 : ans;
+    }
+};
