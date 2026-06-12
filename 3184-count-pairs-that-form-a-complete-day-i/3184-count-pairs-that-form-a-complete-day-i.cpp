c0c3e3c00c8f96b8e8124f8822f13e11db9d8e47
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-12 10:13:05 +0700

    Time: 0 ms (100%), Space: 25.5 MB (7.39%) - LeetHub

---
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        unordered_map<int, long long> um;
+        for(auto& h : hours) um[h % 24]++;
+
+        long long ans = 0;
+        ans += (um[0] * (um[0] - 1) / 2) + (um[12] * (um[12] - 1) / 2);
+        
+        for (int i = 1; i <= 11; i++) ans += um[i] * um[24 - i];
+
+        return ans;
+    }
+};
