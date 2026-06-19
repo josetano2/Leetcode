39bd732946ab5a268f763a85f9ef004ade3dfe25
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-19 16:45:27 +0700

    Time: 1 ms (52.94%), Space: 58.4 MB (11.76%) - LeetHub

---
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.ts
@@ -0,0 +1,6 @@
+function getEncryptedString(s: string, k: number): string {
+    var ss = s + s, ans = "", n = s.length;
+    k = k % n;
+    for(let i = 0; i < n; i++) ans += ss[i + k];
+    return ans;
+};
