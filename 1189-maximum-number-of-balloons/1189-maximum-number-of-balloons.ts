e8e258e96e2f18646ba7da76acf227dab1742b7f
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-22 10:37:10 +0700

    Time: 9 ms (25.27%), Space: 55.9 MB (96.7%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
@@ -1,4 +1,10 @@
 function maxNumberOfBalloons(text: string): number {
     let freq = _.countBy(text);
-    return Math.min(freq['b'] ?? 0, freq['a'] ?? 0, Math.floor((freq['l'] ?? 0) / 2), Math.floor((freq['o'] ?? 0) / 2) ?? 0, freq['n'] ?? 0);
+    return Math.min(
+        freq['b'] ?? 0, 
+        freq['a'] ?? 0, 
+        Math.floor((freq['l'] ?? 0) / 2), 
+        Math.floor((freq['o'] ?? 0) / 2) ?? 0, 
+        freq['n'] ?? 0
+    );
 };
