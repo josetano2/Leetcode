22a480f034de0231da9589d287fa2714025646ae
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-23 07:37:55 +0700

    Time: 1 ms (75%), Space: 57.8 MB (25%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.ts
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.ts
@@ -1,12 +1,12 @@
 function findMinFibonacciNumbers(k: number): number {
-    const fib = [1, 1]
+    const fib = [1, 1];
 
     for(let i = 2; fib[i - 1] < k; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
 
     let ans = 0;
-    for(let i = 99; i >= 0 && k > 0; i--) {
+    for(let i = fib.length; i >= 0 && k > 0; i--) {
         if(k >= fib[i]) {
             k -= fib[i];
             ans++;
