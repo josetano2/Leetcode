27ed91a7de3b7d70624c58810c59173aacf8956c
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-06-28 15:04:42 +0700

    Time: 12 ms (0%), Space: 65.8 MB (100%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.ts
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.ts
@@ -2,9 +2,6 @@ function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
     arr.sort((a, b) => a - b);
 
     arr[0] = 1;
-    for(let i = 1; i < arr.length; i++) {
-        if(Math.abs(arr[i] - arr[i - 1]) <= 1) continue;
-        arr[i] = arr[i - 1] + 1;
-    }
+    for(let i = 1; i < arr.length; i++) if(arr[i] > arr[i - 1]) arr[i] = arr[i - 1] + 1;
     return arr[arr.length - 1];
 };
