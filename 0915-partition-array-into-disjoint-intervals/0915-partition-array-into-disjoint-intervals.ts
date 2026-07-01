2496cfdd226620bb0fd356aa97b66de9e041c2d1
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-07-01 18:20:32 +0700

    Time: 18 ms (25%), Space: 70.6 MB (50%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.ts
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.ts
@@ -1,12 +1,19 @@
 function partitionDisjoint(nums: number[]): number {
-    let lmx = nums[0], cmx = nums[0], partition = 0;
-    for(let i = 1; i < nums.length; i++) {
-        cmx = Math.max(cmx, nums[i]);
+    let n: number = nums.length, pfix: number[] = new Array(n).fill(0), sfix: number[] = new Array(n).fill(0);
 
-        if(nums[i] < lmx) {
-            lmx = cmx;
-            partition = i;
-        }
+    pfix[0] = nums[0];
+    for(let i = 1; i < n; i++) {
+        pfix[i] = Math.max(nums[i], pfix[i - 1]);
     }
-    return partition + 1;
+
+    sfix[n - 1] = nums[n - 1];
+    for(let i = n - 2; i >= 0; i--) {
+        sfix[i] = Math.min(nums[i], sfix[i + 1]);
+    }
+
+    for(let i = 1; i < n; i++) {
+        if(pfix[i - 1] <= sfix[i]) return i;
+    }
+
+    return n;
 };
