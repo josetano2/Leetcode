74b479b67e724d7f29edfff26b45e160fe5cd778
josetano2 <132138270+josetano2@users.noreply.github.com>
2026-07-15 15:10:47 +0700

    Time: 0 ms (100%), Space: 57.8 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.ts
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.ts
@@ -1,10 +1,3 @@
 function gcdOfOddEvenSums(n: number): number {
-    let os = n * n, es = n * (n + 1);
-
-    while (es != 0) {
-        let temp = es;
-        es = os % es;
-        os = temp;
-    }
-    return os;
+    return n;
 };
